b11c8403b13616e53f84e1a14ae2eb6521f891fe
Alexandr <alexraiko2211@gmail.com>
2021-05-18 12:58:09 +0300

    Add project files.

---
+++ Persons Age/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesDemo
+{
+    public class Person
+    {
+        private readonly string firstName;
+        private readonly Gender gender;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+        }
+
+        public string LastName { get; private set; }
+
+        public DateTime Birthday { get; }
+
+        public int Age
+        {
+            get
+            {
+                var now = DateTime.Now;
+                var retYear = now.Year - Birthday.Year;
+                if (Birthday.Month > now.Month)
+                {
+                    retYear--; ;
+                }
+                else if (Birthday.Month == now.Month)
+                {
+                    if (Birthday.Day > now.Day)
+                    {
+                        retYear--;
+                    }
+                }
+                return retYear;
+            }
+        }
+
+        public Person(string firstName, string lastName, DateTime birthday, Gender gender)
+        {
+            this.firstName = firstName;
+            LastName = lastName;
+            Birthday = birthday;
+            this.gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"{ConvertGenderToText()} {FirstName} {LastName} was born in {Birthday.ToString("dd/MMM/yyyy", new CultureInfo("en-GB"))}";
+        }
+
+        private string ConvertGenderToText()
+        {
+            return gender == Gender.Male ? "Mister." : "Miss.";
+        }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ Persons Age/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Alice", "Johnson", new DateTime(1990, 5, 20), Gender.Female);
+            Console.WriteLine(person.ToString());
+            Console.WriteLine("An now she is " + person.Age + " years old");
+        }
+
+        public static void ExceptionExample()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+    }
+}
